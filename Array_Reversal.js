13955ab3e5c3fb14733339adb3f7bd19c78963c3
schauhan <sambhav.singh@quovantis.com>
2019-11-09 01:29:37 +0530

    All file changes (added ques links), with question solutions: sum of nos, simple array sum, array comparison

--- Array_Reversal.js
+++ Array_Reversal.js
@@ -1,3 +1,4 @@
+// https://www.hackerrank.com/challenges/arrays-ds/problem
 // Reverse an Array of integers
 'use strict';
 
--- hello.js
+++ hello.js
@@ -1,1 +1,2 @@
+// https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/hello-32/
 console.log('Hello Kirti')
